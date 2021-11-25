888d7b692daf8b9a2eef7ffd4dd20da2a1a119d2
Jackgoers96 <81663225+Jackgoers96@users.noreply.github.com>
2021-11-24 20:07:09 -0600

    Removed env file

--- stoyocode2-0/client/src/App.js
+++ stoyocode2-0/client/src/App.js
@@ -12,6 +12,7 @@ import Footer from "./components/Footer/Footer";
 import Contact from "./components/Contact/Contact";
 import Resume from "./components/Resume/Resume";
 import "./App.css";
+import S3 from 'react-aws-s3';
 
 const App = () => {
 
@@ -39,3 +40,34 @@ const App = () => {
   );
 };
 export default App;
+
+const config = {
+  bucketName: 'myBucket',
+  dirName: 'media', /* optional */
+  region: 'eu-west-1',
+  accessKeyId: 'JAJHAFJFHJDFJSDHFSDHFJKDSF',
+  secretAccessKey: 'jhsdf99845fd98qwed42ebdyeqwd-3r98f373f=qwrq3rfr3rf',
+  s3Url: 'https:/your-custom-s3-url.com/',
+}
+
+
+// const ReactS3Client = new S3(config);
+// /*  Notice that if you don't provide a dirName, the file will be automatically uploaded to the root of your bucket */
+
+// /* This is optional */
+// const newFileName = 'test-file';
+
+// ReactS3Client
+//   .uploadFile(file, newFileName)
+//   .then(data => console.log(data))
+//   .catch(err => console.error(err))
+
+
+// {
+//   Response: {
+//     bucket: "myBucket",
+//       key: "image/test-image.jpg",
+//         location: "https://myBucket.s3.amazonaws.com/media/test-file.jpg"
+//   }
+// }
+
