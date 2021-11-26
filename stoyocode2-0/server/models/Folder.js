b757065d5d4aed03896f9e107e0c5d60e87c8fe5
Shawn McWhorter <73659834+Rhandom1@users.noreply.github.com>
2021-11-26 11:28:26 -0600

    fixed syntax issues in models and added typeDefs

--- stoyocode2-0/server/models/Folder.js
+++ stoyocode2-0/server/models/Folder.js
@@ -5,13 +5,19 @@ const folderSchema = new Schema({
         type: String,
         required: "You need a title for this folder.",
         trim: true
-    }
+    },
 
     folderAuthor: {
         type: String,
         required: true,
         trim: true
-    }
+    },
+    snippet: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Snippet',
+        },
+      ],
 })
 
 const Folder = model('Folder', folderSchema);
--- stoyocode2-0/server/models/User.js
+++ stoyocode2-0/server/models/User.js
@@ -19,6 +19,12 @@ const userSchema = new Schema({
     required: true,
     minlength: 5,
   },
+  folder:[
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Folder'
+    },
+  ],
   snippet: [
     {
       type: Schema.Types.ObjectId,
