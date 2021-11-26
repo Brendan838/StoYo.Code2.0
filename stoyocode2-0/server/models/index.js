494e33e34678acfe3e6d5d074b886cb87309446b
Shawn McWhorter <73659834+Rhandom1@users.noreply.github.com>
2021-11-26 09:48:26 -0600

    Set up models and config
    Do we want to have a comment section for the snippets? I have removed for now but easy to replace.

--- stoyocode2-0/server/models/index.js
+++ stoyocode2-0/server/models/index.js
@@ -1,4 +1,4 @@
 const User = require('./User');
-const Thought = require('./Thought');
+const Thought = require('./Snippet');
 
-module.exports = { User, Thought };
+module.exports = { User, Snippet };
